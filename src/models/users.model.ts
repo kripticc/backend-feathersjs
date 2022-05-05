8ef059a4f130cd86c27956f7381ae0348cf1e379
Uditt Lamba <uditt@aetho.co>
2022-05-05 09:53:33 +0530

    setup postgres docker-compose and migration through sequelize.

--- src/models/users.model.ts
+++ src/models/users.model.ts
@@ -1,46 +1,37 @@
-// See https://express-cassandra.readthedocs.io/en/latest/schema/
+// See https://sequelize.org/master/manual/model-basics.html
 // for more of what you can do here.
-import { Application } from '../declarations'
+import { Sequelize, DataTypes, Model } from 'sequelize';
+import { Application } from '../declarations';
+import { HookReturn } from 'sequelize/types/hooks'
 
-export default function (app: Application): any {
-  const models = app.get('models')
-  const users = models.loadSchema('users', {
-    table_name: 'users',
-    fields: {
-      id: 'int',
+export default function (app: Application): typeof Model {
+  const sequelizeClient: Sequelize = app.get('sequelizeClient');
+  const users = sequelizeClient.define('users', {
 
-      email: 'text',
-      password: {
-        type: 'text',
-        rule: {
-          required: true
-        }
-      }
-
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
     },
-    key: ['id'],
-    custom_indexes: [
-      {
-        on: 'email',
-        using: 'org.apache.cassandra.index.sasi.SASIIndex',
-        options: {}
-      },
-      {
-        on: 'password',
-        using: 'org.apache.cassandra.index.sasi.SASIIndex',
-        options: {}
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+
+
+  }, {
+    hooks: {
+      beforeCount(options: any): HookReturn {
+        options.raw = true;
       }
-    ],
-    options: {
-      timestamps: true
     }
-  }, function (err: any) {
-    if (err) throw err
-  })
+  });
 
-  users.syncDB(function (err: any) {
-    if (err) throw err
-  })
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  (users as any).associate = function (models: any): void {
+    // Define associations here
+    // See https://sequelize.org/master/manual/assocs.html
+  };
 
-  return users
+  return users;
 }
